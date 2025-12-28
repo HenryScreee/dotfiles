738e5bf2e5abf244da0624fc623a725dfa18c37c
git push -u origin mainHenryScreee <echo "Pushing with embedded token...>
2025-12-27 19:27:24 -0500

    Fix: Force add config files bypassing .gitignore

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     set -g fish_greeting
     if type -q fastfetch
         fastfetch
     end
 end
-
-# Environment variables
-set -gx EDITOR nano
-set -gx VISUAL nano
